6b7cad837afc0138edb48db03a6754fad4b1627f
Joost Grunwald <joostgrunwald2001@gmail.com>
2022-04-06 14:46:31 +0200

    added sorting and slicing

--- domainfinder.py
+++ domainfinder.py
@@ -10,6 +10,8 @@ def main(word):
     #get all subfolders of current directory
     subfolders = [ f.path for f in os.scandir(cwd) if f.is_dir() ]
 
+    outputlist = []
+
     #traverse all files inside subfolders of the current directory
     for folder in subfolders:
         for filename in os.listdir(folder):
@@ -22,9 +24,19 @@ def main(word):
                 file = open(f, 'r')
                 text = file.read()
 
-                #find word in file
-                if text.find(word) != -1:
-                    
+                var1 = " " + word + " "
+                var2 = ";" + word + ";"
+                var3 = " " + word + ";"
+                var4 = ";" + word + " "
+
+                count1 = text.count(var1)
+                count2 = text.count(var2)
+                count3 = text.count(var3)
+                count4 = text.count(var4)
+
+                totalcount = count1 + count2 + count3 + count4
+                if totalcount != 0:
+
                     #adjust word
                     f = f.replace(str(cwd),"")
                     f = f[1:]
@@ -34,9 +46,22 @@ def main(word):
                         slash = f.find("\\")
                         if slash != -1:
                             f = f[slash+1:]
-                            
-                    print(f)
-                    print(text.count(word))
 
-word = input("Select a word to find domains for: \n")
-main(word)
+                    if f.find("factotum") == -1:
+                        outputlist.append((f, totalcount))
+                    #print(f'{f}: occurrences: {totalcount}')
+
+    #sort the list on value
+    outputlist.sort(key=lambda y: y[1])
+
+    #reverse it to start high
+    outputlist.reverse()
+
+    #print list
+    for occ in outputlist[:5]:
+        print(occ)
+
+while True:
+    word = input("Select a word to find domains for: \n")
+    main(word)
+    print("")
