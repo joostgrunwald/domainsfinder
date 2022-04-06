af6f956595ee4771bcf3f34905fca62fdfad4fa6
Joost Grunwald <joostgrunwald2001@gmail.com>
2022-04-06 13:32:41 +0200

    added parsing of output

--- domainfinder.py
+++ domainfinder.py
@@ -4,19 +4,39 @@ import os
 
 def main(word):
 
+    #get current directory
     cwd = os.getcwd()
 
+    #get all subfolders of current directory
     subfolders = [ f.path for f in os.scandir(cwd) if f.is_dir() ]
 
+    #traverse all files inside subfolders of the current directory
     for folder in subfolders:
         for filename in os.listdir(folder):
             f = os.path.join(folder, filename)
             
-            # checking if it is a file
+            # checking if it is a txt file
             if os.path.isfile(f) and f.find(".txt") != -1:
-                text = f.read()
+
+                #open file and read it
+                file = open(f, 'r')
+                text = file.read()
+
+                #find word in file
                 if text.find(word) != -1:
+                    
+                    #adjust word
+                    f = f.replace(str(cwd),"")
+                    f = f[1:]
+                    f = f.replace(".txt","")
+
+                    if len(os.listdir(folder)) == 1:
+                        slash = f.find("\\")
+                        if slash != -1:
+                            f = f[slash+1:]
+                            
                     print(f)
+                    print(text.count(word))
 
 word = input("Select a word to find domains for: \n")
 main(word)
